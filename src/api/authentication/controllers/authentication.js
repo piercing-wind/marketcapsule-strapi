18d4c3fb7364c5a1decc6a54a8324da3d330dae2
Rahul kumar <rahul.kumar@valuecreatives.com>
2024-12-24 13:26:29 +0530

    commit changes

--- src/api/authentication/controllers/authentication.js
+++ src/api/authentication/controllers/authentication.js
@@ -580,7 +580,7 @@ async function verifyFacebookToken(token, provider, type) {
   try {
     console.log("in facebook veirfy fnn")
     const response = await axios.get(
-      `https://graph.facebook.com/me?access_token=${token}&fields=id,email`
+      `https://graph.facebook.com/me?access_token=${token}&fields=id,email,public_profile`
     );
     const userData = response.data;
 
