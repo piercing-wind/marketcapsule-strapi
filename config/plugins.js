3140852ff97dcdad8609dd98f007c08f7a476d9e
piercing-wind <sourabhsharma5677@gmail.com>
2025-01-25 15:16:13 +0530

    Changes in Plugin.js for aws s3

--- config/plugins.js
+++ config/plugins.js
@@ -6,11 +6,8 @@
           sizeLimit: 5000 * 1024 * 1024, // 5GB
           provider: 'aws-s3',
           providerOptions: {
-            s3Options:{
-              credentials: {
-                accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                secretAccessKey: env('AWS_ACCESS_SECRET'),
-              },
+              accessKeyId: env('AWS_ACCESS_KEY_ID'),
+              secretAccessKey: env('AWS_ACCESS_SECRET'),
               region: env('AWS_REGION'),
               baseUrl: env('AWS_CLOUDFRONT_BASE_URL'),
               params: {
@@ -18,7 +15,6 @@
                 signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
                 Bucket: env('AWS_BUCKET'),
               },
-            },
           },
           actionOptions: {
             upload: {},
